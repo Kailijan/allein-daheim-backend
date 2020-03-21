a88f87c17cb23189b13765fc62c192aff7ea3435
kailijan <kai.frankenhaeuser@t-online.de>
2020-03-21 09:18:44 +0100

    Init spring boot project

---
+++ src/main/java/de/team_franky/allein_daheim/AlleinDaheimApplication.java
@@ -0,0 +1,13 @@
+package de.team_franky.allein_daheim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlleinDaheimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlleinDaheimApplication.class, args);
+	}
+
+}
